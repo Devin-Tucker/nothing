d2a4620c31e5a1b93bc64d0416052852d673061e
Devin_Tucker <DevinTucker311@gmail.com>
2024-04-19 17:52:02 -0500

    push

--- 3dtest1.py
+++ 3dtest1.py
@@ -5,11 +5,15 @@ from ursina import *
 app = ursina.Ursina()
 app = Ursina()
 
-# Create a cube
-cube = Entity(model='cube', color=color.red, scale=(1, 1, 1))
+list()
+for i in range(5):
+        cube = Entity(model='cube', color=color.red, scale=(1, 1, 1))
+        cube.x = i * 2
 
-# Create a sphere
-sphere = Entity(model='sphere', color=color.blue, scale=(1, 1, 1))
+list()
+for i in range(10):
+        sphere = Entity(model = 'sphere', color=color.green, scale=(2, 2, 2))
+        sphere.y = i * 4
 
 # Create a camera
 camera = EditorCamera()
