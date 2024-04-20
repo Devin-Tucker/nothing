88a59c822c23524836c4b630e3e0a55cbf166a88
Devin_Tucker <DevinTucker311@gmail.com>
2024-04-20 10:20:55 -0500

    Camera

--- 3dtest1.py
+++ 3dtest1.py
@@ -1,6 +1,6 @@
 import ursina
 from ursina import *
-
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = ursina.Ursina()
 app = Ursina()
@@ -15,22 +15,9 @@ for i in range(10):
         sphere = Entity(model = 'sphere', color=color.green, scale=(2, 2, 2))
         sphere.y = i * 4
 
-# Create a camera
-camera = EditorCamera()
 
-def update():
-    # Move the camera
-    camera.y += held_keys['e'] * time.dt
-    camera.y -= held_keys['q'] * time.dt
-    camera.x += held_keys['d'] * time.dt
-    camera.x -= held_keys['a'] * time.dt
-    camera.z += held_keys['w'] * time.dt
-    camera.z -= held_keys['s'] * time.dt
-
-def update():
-    camera.rotation_y += held_keys['right arrow'] * 1
-    camera.rotation_y -= held_keys['left arrow'] * 1
-    camera.rotation_x += held_keys['up arrow'] * 1
-    camera.rotation_x -= held_keys['down arrow'] * 1
+player = FirstPersonController()
+player.gravity = 0
+player.mouse_sensitivity = Vec3(80 ,80 ,80)
 
 app.run()
