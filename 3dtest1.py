0eb1260293f2af78dd2529a6970c85260a3e1f26
Devin_Tucker <DevinTucker311@gmail.com>
2024-04-21 16:03:35 -0500

    parkor

--- 3dtest1.py
+++ 3dtest1.py
@@ -5,19 +5,20 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = ursina.Ursina()
 app = Ursina()
 
-list()
-for i in range(5):
-        cube = Entity(model='cube', color=color.red, scale=(1, 1, 1))
-        cube.x = i * 2
+for z in range(10):
+    for x in range(10):
+        cube = Entity(
+            model='cube', color=color.gray, position = (x, 0, z), parent=scene, origin_y=0.5, texture='grass')
+        cube.collider = BoxCollider(cube, center=Vec3(0,0.5,0), size=Vec3(1,1,1))
 
-list()
-for i in range(10):
-        sphere = Entity(model = 'sphere', color=color.green, scale=(2, 2, 2))
-        sphere.y = i * 4
-
+for z in range(10):
+    for x in range(10):
+        for y in range(10):
+            cube = Entity(model='cube', color=color.gray, position = (x+12+2*x, y-1-y, z), parent=scene, origin_y=1, texture='grass', collider = 'box')
+            cube.collider = BoxCollider(cube, center=Vec3(0,0.5,0), size=Vec3(1,1,1))
 
 player = FirstPersonController()
-player.gravity = 0
+player.gravity = 0.3
 player.mouse_sensitivity = Vec3(80 ,80 ,80)
 
 app.run()
