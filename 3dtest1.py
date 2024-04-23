e1aaf3bf8d40f12c758baa8865f92d44c08511eb
Devin_Tucker <devin.tucker.2@us.af.mil>
2024-04-23 10:15:33 -0500

    gun

--- 3dtest1.py
+++ 3dtest1.py
@@ -29,8 +29,34 @@ player = FirstPersonController()
 player.gravity = 0.5
 player.mouse_sensitivity = Vec3(80 ,80 ,80)
 
+
+gun = Entity(model='cube', color=color.gold, position=(1, 1, 1), parent=player, scale=(0.5, 0.2, 1))
+gun_model = load_model('path_to_your_gun_model')
+gun.scale = (0.5, 0.2, 1)
+
+class Bullet(Entity):
+    def __init__(self, direction, speed, lifetime=2):
+        super().__init__(model='sphere', color=color.red, position=gun.world_position, scale=0.1)
+
+        self.velocity = direction * speed
+        self.lifetime = lifetime
+
+    def update(self):
+        self.position += self.velocity * time.dt
+        self.lifetime -= time.dt
+
+        if self.lifetime <= 0:
+            destroy(self)
+
+def input(key):
+    if key == 'left mouse down':
+        bullet = Bullet(direction=gun.forward, speed=10)
+
 def update():
     if held_keys['r']:
         player.y = 20
+
+
+
 
 app.run()
